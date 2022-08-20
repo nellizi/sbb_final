ca6069dad097b498eeb7cff30d8c1e5e0c739a52
nellizi <masterliz9095@gmail.com>
2022-08-20 22:15:49 +0900

    세션 저장, 세션에서 값 불러오기

--- src/main/java/com/ll/exam/sbbfinal/MainController.java
+++ src/main/java/com/ll/exam/sbbfinal/MainController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -13,6 +14,7 @@ import java.util.stream.IntStream;
 public class MainController {
 
     private int i = 0;
+
     @RequestMapping("/sbb")
     // 아래 함수의 리턴값을 그대로 브라우저에 표시
     // 아래 함수의 리턴값을 문자열화 해서 브라우저 응답의 바디에 담는다.
@@ -55,8 +57,8 @@ public class MainController {
 
     @GetMapping("plus")
     @ResponseBody
-    public int plus(int a, int b){
-    return a+b;
+    public int plus(int a, int b) {
+        return a + b;
     }
 
     @GetMapping("/plus2")
@@ -65,35 +67,35 @@ public class MainController {
         int a = Integer.parseInt(request.getParameter("a"));
         int b = Integer.parseInt(request.getParameter("b"));
 
-        response.getWriter().append(a+b+"");
+        response.getWriter().append(a + b + "");
     }
 
     @GetMapping("/gugudan")
     @ResponseBody
-    public String showGugudan(Integer dan, Integer limit){
-        if(limit == null){
+    public String showGugudan(Integer dan, Integer limit) {
+        if (limit == null) {
             limit = 9;
         }
-        if(dan == null){
+        if (dan == null) {
             dan = 9;
         }
-        Integer finalDan=dan;
-        return IntStream.rangeClosed(1,limit)
-                .mapToObj(i->"%d*%d=%d".formatted(finalDan,i,finalDan*i))
+        Integer finalDan = dan;
+        return IntStream.rangeClosed(1, limit)
+                .mapToObj(i -> "%d*%d=%d".formatted(finalDan, i, finalDan * i))
                 .collect(Collectors.joining("<br>"));
     }
 
     @GetMapping("/mbti/{name}")
     @ResponseBody
-    public String showMbti(@PathVariable String name){
-        String rs = switch (name){
+    public String showMbti(@PathVariable String name) {
+        String rs = switch (name) {
             case "홍길동" -> "INFT";
             case "홍길순" -> "INFJ";
             case "임꺽정" -> "ENFJ";
-            case "이지현","제제" -> "ENFP";
+            case "이지현", "제제" -> "ENFP";
             default -> "CUTE";
         };
-    return rs;
+        return rs;
     }
 //    return switch(name){
 //        case "홍길동" -> "INFT";
@@ -102,4 +104,20 @@ public class MainController {
 //        case "이지현","제제" -> "ENFP";
 //        default -> "CUTE";
 //    };         이렇게 바로 리턴 뒤에 작성하는것도 가능
+
+    @GetMapping("/saveSession/{name}/{value}")
+    @ResponseBody
+    public String saveSession(@PathVariable String name, @PathVariable String value, HttpServletRequest req) {
+        HttpSession session = req.getSession(); //req 안에 쿠키가 있다 => req에서 세션 얻기 가능 . 요청 올 때 쿠키가 들어온다. (JSESSIONID)
+        session.setAttribute(name, value);
+        return "세션변수 %s의 값이 %s로 설정되었습니다.".formatted(name, value);
+
+    }
+
+    @GetMapping("/getSession/{name}")
+    @ResponseBody
+    public String getSession(@PathVariable String name, HttpSession session) {
+        String value = (String) session.getAttribute(name);
+        return "세션변수 %s의 값이 %s 입니다.".formatted(name, value);
+    }
 }
