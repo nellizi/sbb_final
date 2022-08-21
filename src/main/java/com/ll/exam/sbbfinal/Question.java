0ac52e1b53aa8f53e9e28da428ac33c8290a563b
nellizi <masterliz9095@gmail.com>
2022-08-21 18:38:45 +0900

    객체지향적으로 저장

--- src/main/java/com/ll/exam/sbbfinal/Question.java
+++ src/main/java/com/ll/exam/sbbfinal/Question.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -23,6 +24,12 @@ public class Question {
 
     private LocalDateTime createDate;
 
-    @OneToMany(mappedBy = "question" , cascade = CascadeType.REMOVE, fetch = FetchType.EAGER) //mappesBy : answer엔티티에 쓰인 변수명
-    private List<Answer> answerList;  //클래스에만 존재하는 것
+    @OneToMany(mappedBy = "question" , cascade = CascadeType.REMOVE) //mappesBy : answer엔티티에 쓰인 변수명
+    private List<Answer> answerList = new ArrayList<>();  //클래스에만 존재하는 것
+
+    public void addAnswer(Answer answer) {
+
+        answer.setQuestion(this);
+        getAnswerList().add(answer);
+    }
 }
--- src/test/java/com/ll/exam/sbbfinal/AnswerRepositoryTests.java
+++ src/test/java/com/ll/exam/sbbfinal/AnswerRepositoryTests.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 
+import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -34,33 +36,36 @@ public class AnswerRepositoryTests {
         answerRepository.truncateTable();
     }
 
+
     private void createSampleData() {
         QuestionRepositoryTests.createSampleData(questionRepository);
         Question q = questionRepository.findById(1).get();
 
         Answer a1 = new Answer();
         a1.setContent("sbb는 질문답변 게시판 입니다.");
-        a1.setQuestion(q);
         a1.setCreateDate(LocalDateTime.now());
+        q.addAnswer(a1);
         answerRepository.save(a1);
-
 
         Answer a2 = new Answer();
         a2.setContent("sbb에서는 주로 스프링부트관련 내용을 다룹니다.");
-        a2.setQuestion(q);
         a2.setCreateDate(LocalDateTime.now());
+        q.addAnswer(a2);
         answerRepository.save(a2);
 
-    }
+        questionRepository.save(q);
 
+    }
+    @Transactional
+    @Rollback(false)
     @Test
     void 저장() {
         Question q = questionRepository.findById(2).get();
 
         Answer a = new Answer();
         a.setContent("네 자동으로 생성됩니다.");
-        a.setQuestion(q);
         a.setCreateDate(LocalDateTime.now());
+        q.addAnswer(a);
         answerRepository.save(a);
     }
     @Test
@@ -70,6 +75,8 @@ public class AnswerRepositoryTests {
         assertThat(a.getContent()).isEqualTo("sbb는 질문답변 게시판 입니다.");
     }
     @Test
+    @Transactional
+    @Rollback(false)
     void  연관_question_조회() {
 
         Answer a = this.answerRepository.findById(1).get();
@@ -77,8 +84,11 @@ public class AnswerRepositoryTests {
         assertThat(question.getId()).isEqualTo(1);
     }
     @Test
+    @Transactional
+    @Rollback(false)
     void  question으로_answer조회() {
         Question question = questionRepository.findById(1).get();
+
 
         List<Answer> answerList = question.getAnswerList();
 
