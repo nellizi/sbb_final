981ec5b8817b00741699850dbd70b1c17505dfba
nellizi <masterliz9095@gmail.com>
2022-08-21 23:15:12 +0900

    QuestionController 추가

---
+++ src/main/java/com/ll/exam/sbbfinal/question/QuestionController.java
@@ -0,0 +1,15 @@
+package com.ll.exam.sbbfinal.question;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class QuestionController {
+    @RequestMapping("/question/list")
+    @ResponseBody
+    public String list() {
+        return "question list";
+    }
+}
