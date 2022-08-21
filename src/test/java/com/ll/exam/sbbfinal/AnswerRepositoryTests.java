ba395cd56a57e59db5ae8f553bd212a910fe5aa5
nellizi <masterliz9095@gmail.com>
2022-08-21 14:40:58 +0900

    answer조회 테스트

--- src/test/java/com/ll/exam/sbbfinal/AnswerRepositoryTests.java
+++ src/test/java/com/ll/exam/sbbfinal/AnswerRepositoryTests.java
@@ -7,6 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 public class AnswerRepositoryTests {
@@ -30,6 +35,21 @@ public class AnswerRepositoryTests {
 
     private void createSampleData() {
         QuestionRepositoryTests.createSampleData(questionRepository);
+        Question q = questionRepository.findById(1).get();
+
+        Answer a1 = new Answer();
+        a1.setContent("sbb는 질문답변 게시판 입니다.");
+        a1.setQuestion(q);
+        a1.setCreateDate(LocalDateTime.now());
+        answerRepository.save(a1);
+
+
+        Answer a2 = new Answer();
+        a2.setContent("sbb에서는 주로 스프링부트관련 내용을 다룹니다.");
+        a2.setQuestion(q);
+        a2.setCreateDate(LocalDateTime.now());
+        answerRepository.save(a2);
+
     }
 
     @Test
@@ -42,4 +62,11 @@ public class AnswerRepositoryTests {
         a.setCreateDate(LocalDateTime.now());
         answerRepository.save(a);
     }
+    @Test
+    void 조회() {
+
+        Answer a = this.answerRepository.findById(1).get();
+        assertThat(a.getContent()).isEqualTo("sbb는 질문답변 게시판 입니다.");
+    }
+
 }
