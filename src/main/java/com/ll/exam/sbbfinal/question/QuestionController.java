10de2848779e1989f95d1683d39bfa659d0f7365
nellizi <masterliz9095@gmail.com>
2022-08-23 00:59:52 +0900

    질문 폼 만들기

--- src/main/java/com/ll/exam/sbbfinal/question/QuestionController.java
+++ src/main/java/com/ll/exam/sbbfinal/question/QuestionController.java
@@ -4,6 +4,7 @@ package com.ll.exam.sbbfinal.question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -30,5 +31,9 @@ public class QuestionController {
         model.addAttribute("question",question);
 
       return "question_detail";
+    }
+    @GetMapping("/create")
+    public String questionCreate() {
+        return "question_form";
     }
 }
