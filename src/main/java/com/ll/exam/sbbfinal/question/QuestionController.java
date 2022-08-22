95281a5f65ebfe24c510f8b2bd12d04346d78b09
nellizi <masterliz9095@gmail.com>
2022-08-23 01:05:48 +0900

    질문 등록 처리

--- src/main/java/com/ll/exam/sbbfinal/question/QuestionController.java
+++ src/main/java/com/ll/exam/sbbfinal/question/QuestionController.java
@@ -4,10 +4,7 @@ package com.ll.exam.sbbfinal.question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RequiredArgsConstructor
@@ -35,5 +32,10 @@ public class QuestionController {
     @GetMapping("/create")
     public String questionCreate() {
         return "question_form";
+    }
+    @PostMapping("/create")
+    public String questionCreate(String subject, String content) {
+        questionService.create(subject,content);
+        return "redirect:/question/list";
     }
 }
--- src/main/java/com/ll/exam/sbbfinal/question/QuestionService.java
+++ src/main/java/com/ll/exam/sbbfinal/question/QuestionService.java
@@ -4,6 +4,7 @@ import com.ll.exam.sbbfinal.DataNotFoundExceprion;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,5 +22,13 @@ public class QuestionService {
     public Question getQuestion(int id) {
      return questionRepository.findById(id)
              .orElseThrow(() -> new DataNotFoundExceprion("no %d question".formatted(id)));
+   }
+
+    public void create(String subject, String content) {
+       Question q = new Question();
+       q.setSubject(subject);
+       q.setContent(content);
+       q.setCreateDate(LocalDateTime.now());
+       questionRepository.save(q);
    }
 }
